0882513eaa55b2c922248cbea7742449e3b6d09d
Hoangkhaccuong <khaccuong.997@gmail.com>
2021-09-07 14:41:24 +0700

    part_678

--- src/views/Example.js
+++ src/views/Example.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react'
 
-export default class Example extends Component {
-    render() {
-        return (
-            <>
-            <div>
-                <label>User Name</label>
-                <input type='text'/>
-             </div>
-             <div>   
-                <label>Password</label>
-                <input type='password'/>
 
-            </div>
+export default class Example extends Component {
+  state={
+    name:'Ci',
+    age:18,
+    add:'QB'        
+  }
+// handleOnInput =(event)=>{
+//     this.setState({
+//       name:event.target.value
+//     })
+// }
+handleSsubmit=()=>{
+  alert('sumbit')
+}
+handleOnChangeInput=(e)=>{
+  this.setState({
+    name:e.target.value,
+    age:11,
+    add:"QN",
+    
+  })
+  console.log(e.target.value ,'event',e.target ,e,'')
+}
+    render() {
+        const age=17
+        console.log('hoc vs react ', this.state)
+        return (
+          
+           <>
+           <div>
+             <h1>Login {this.state['name']} </h1>
+             <input type='text' value={this.name} onChange={(e)=>this.handleOnChangeInput(e)}></input>
+              <button onClick={()=>this.handleSsubmit()}>Submit</button>
+           </div>
             </>
         )
     }
